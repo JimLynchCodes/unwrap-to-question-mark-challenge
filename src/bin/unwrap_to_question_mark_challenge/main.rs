1a0dfdc4b88cc3dffc0b793f012b432dca65887e
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2023-03-28 13:17:26 -0400

    unwraps everywhere

--- src/bin/unwrap_to_question_mark_challenge/main.rs
+++ src/bin/unwrap_to_question_mark_challenge/main.rs
@@ -2,6 +2,6 @@ mod api_caller;
 use api_caller::get_some_json;
 
 fn main() {    
-    let dynamic_json = get_some_json().expect("Calling for json failed. 😔");
+    let dynamic_json = get_some_json().unwrap();
     println!("some json: {:#?}", dynamic_json);
 }
